8d77afe03c9397c03458a993700b2cd96ca771e5
Wojciech Skirło <w.skirlo@ideo.pl>
2024-10-08 21:40:41 +0200

    chore: add dotenv library

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import dotnev from 'dotenv';
 
 const app = express();
+// const router = express.Router();
 
+dotnev.config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
+// app.use('/', router);
 
-console.log("asdasdaa sdasdas dbuild asd asd ads ds")
+app.get('/', (req, res) => {
+    // res.send('Hello World!');
+    res.json({ message: 'Hello World!' });
+});
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 4000;
+const PORT = +(process.env.PORT || 3000);
+const HOST_NAME = process.env.HOST_NAME || 'localhost';
 
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+server.listen(PORT, HOST_NAME, () => {
+    console.log(`Server listening on port ${HOST_NAME}:${PORT}`);
 });
