6f235fccdb7fc5868f714a85d810ade8fb8dd51d
Wojciech Skirło <w.skirlo@ideo.pl>
2024-10-11 19:53:09 +0200

    ci: change from netlify to render

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
-// import http from 'http';
+import http from 'http';
 import cors from 'cors';
 import dotnev from 'dotenv';
-import serverless from "serverless-http";
 import AuthRoutes from './routes/Auth.routes';
 
 dotnev.config();
 
-// const supabase = getSupabase();
 const app = express();
 const router = express.Router();
 
@@ -22,11 +20,11 @@ app.get('/', async (_, res) => {
     res.json({ message: 'Festivente!' });
 });
 
-export const handler = serverless(app);
+const server = http.createServer(app);
+const PORT = process.env.PORT || 4000;
 
-// const server = http.createServer(app);
-// const PORT = +(process.env.PORT || 3000);
+server.listen(PORT, () => {
+	console.log(`Server listening on port http://localhost:${PORT}`);
+});
 
-// server.listen(PORT, () => {
-//     console.log(`Server listening on port http://localhost:${PORT}`);
-// });
+export default app;
