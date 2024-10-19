a4eaaab402d0bae78143ccd655c5917d2f1465b8
Wojciech Skirło <w.skirlo@ideo.pl>
2024-10-19 22:20:04 +0200

    feat(user): add list endpoint

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -18,4 +18,22 @@ export default class UserController {
       res.status(500).json({ message: 'Internal server error' });
     }
   }
+
+  static async list(req: Request, res: Response): Promise<void> {
+    try {
+      const { data: { users }, error } = await supabase.auth.admin.listUsers()
+      // const { data, error } = await supabase.from('users').select('*');
+
+      console.log("data", error);
+
+      if (error) {
+        res.status(400).json({ message: 'Invalid access token' });
+        return;
+      }
+
+      res.status(200).json({ data: users, message: 'Udało się pobrać listę użytkowników :)' });
+    } catch (err) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
 }
--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -4,5 +4,6 @@ import UserController from '../controllers/User.controller';
 const router = Router();
 
 router.get('/me', UserController.me);
+router.get('/list', UserController.list);
 
 export default router;
